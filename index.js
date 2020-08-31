e1efd7eb6824db1ac37f8bc35278bbd3a047dc8e
Alex-La <lars6162@gmail.com>
2020-09-01 00:46:12 +0300

    ready

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.get("/mailru-domaindNzorCXbwbEMzsCI.html", (_, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "public", "mailru-domaindNzorCXbwbEMzsCI.html")
+  );
+});
+
+app.listen({ port: 4000 }, () => {
+  "Server ready";
+});
